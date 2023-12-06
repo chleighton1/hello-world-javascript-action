c1da2accd2d2907b18d061d27dd5bbd1701d3d82
Charles Leighton <chleighton1@gmail.com>
2023-12-06 16:05:01 -0500

    adjusted to use notion

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Client } = require("@notionhq/client");
+require("dotenv").config();
 
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+// async function getCurrentSprint(SPRINT_DB_ID) {
+//   const response = await notion.databases.query({
+//     database_id: SPRINT_DB_ID,
+//   });
+//   const currentSprint = response.results.find((sprint) => {
+//     return sprint.properties["Sprint status"].status.id === "current";
+//   });
+//   return currentSprint;
+// }
+
+async function getTaskDB(TASK_DB_ID) {
+  const response = await notion.databases.query({
+    database_id: TASK_DB_ID,
+  });
+
+  return response;
+}
+
+const number = 567;
+
+getTaskDB(process.env.TASK_DB_ID)
+  .then((response) => {
+    const results = response.results;
+    const task = results.find(
+      (page) => page.properties["Task ID"].unique_id.number === number
+    );
+    console.log(task);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
